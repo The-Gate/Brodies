a437155a140697565eec61fbcc1bbc2437dec06f
Callum Elliot <callum@sakurabrae.co.uk>
2017-01-03 15:12:55 +0000

    business sector / legal services / binformed menu styled

--- sites/all/themes/brodies201612/template.php
+++ sites/all/themes/brodies201612/template.php
@@ -3,3 +3,59 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+// remove the boostrap dropdown menu
+function brodies201612_menu_link__menu_block($variables) {
+  $keep_bootstrap_array = array('menu_link__menu_block__3');
+  if (in_array($variables['element']['#theme'][0], $keep_bootstrap_array)) {
+    return brodies201612_bootstrap_menu_link($variables);
+  }
+  else {
+    return theme_menu_link($variables);
+  }
+}
+
+
+function brodies201612_bootstrap_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  $title = $element['#title'];
+  $href = $element['#href'];
+  $options = !empty($element['#localized_options']) ? $element['#localized_options'] : array();
+  $attributes = !empty($element['#attributes']) ? $element['#attributes'] : array();
+
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+      $sub_menu = '<a class="dropdown-caret dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a><ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+
+      // Generate as standard dropdown.
+      $title .= ' ';
+      $attributes['class'][] = 'dropdown';
+
+      $options['html'] = TRUE;
+
+      // Set dropdown trigger element to # to prevent inadvertant page loading
+      // when a submenu link is clicked.
+      //$options['attributes']['data-target'] = '#';
+      $options['attributes']['class'][] = 'dropdown-li';
+      //$options['attributes']['class'][] = 'dropdown-toggle';
+      //$options['attributes']['data-toggle'] = 'dropdown';
+    }
+  }
+
+  // Filter the title if the "html" is set, otherwise l() will automatically
+  // sanitize using check_plain(), so no need to call that here.
+  if (!empty($options['html'])) {
+    $title = _bootstrap_filter_xss($title);
+  }
+
+  return '<li' . drupal_attributes($attributes) . '>' . l($title, $href, $options) . $sub_menu . "</li>\n";
+}
