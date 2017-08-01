95d21c74170d57f3af71a4516bfa043188697392
Callum Elliot <callum@sakurabrae.co.uk>
2017-08-01 12:42:22 +0100

    Drupal security update:
    - core update;
    - maintenance page updated

--- index-maintenance.php
+++ index-maintenance.php
@@ -1,66 +1,51 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="shortcut icon" href="/misc/favicon.ico" type="image/x-icon" />
-<meta name="revisit-after" content="1 day" />
-    <title>Site off-line | Brodies LLP</title>
-    <link type="text/css" rel="stylesheet" media="all" href="/modules/node/node.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/modules/system/defaults.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/modules/system/system.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/modules/system/system-menus.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/modules/user/user.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/cck/theme/content-module.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/ckeditor/ckeditor.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/date/date.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/date/date_popup/themes/datepicker.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/date/date_popup/themes/jquery.timeentry.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/eu-cookie-compliance/css/eu_cookie_compliance.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/filefield/filefield.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/cck/modules/fieldgroup/fieldgroup.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/sites/all/modules/views/css/views.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/modules/system/maintenance.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/themes/garland/style.css?t" />
-<link type="text/css" rel="stylesheet" media="all" href="/themes/garland/minnelli/minnelli.css?t" />
-<link type="text/css" rel="stylesheet" media="print" href="/themes/garland/print.css?t" />
-    <script type="text/javascript" src="/misc/jquery.js?t"></script>
-<script type="text/javascript" src="/misc/drupal.js?t"></script>
-<script type="text/javascript" src="/sites/all/modules/poormanscron/poormanscron.js?t"></script>
-<script type="text/javascript">
-<!--//--><![CDATA[//><!--
-jQuery.extend(Drupal.settings, { "basePath": "/", "cron": { "basePath": "/poormanscron", "runNext": 1445365953 } });
-//--><!]]>
-</script>
-    <!--[if lt IE 7]>
-      <link type="text/css" rel="stylesheet" media="all" href="/themes/garland/fix-ie.css" />    <![endif]-->
-  </head>
-  <body>
 
-<!-- Layout -->
-  <div id="header-region" class="clear-block"></div>
+<head>
+  <title>Site under maintenance | Brodies LLP</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="/sites/all/themes/brodies201612/favicon.ico" type="image/vnd.microsoft.icon" />
+  <link type="text/css" rel="stylesheet" href="/sites/all/themes/brodies201612/css/style.css" media="all" /></head>
+<body class="maintenance-page in-maintenance no-sidebars">
+  <div id="page">
+    <div id="header">
+      <div id="logo-title">
 
-    <div id="wrapper">
-    <div id="container" class="clear-block">
-
-      <div id="header">
-        <div id="logo-floater">
-        <h1><a href="/" title="Brodies LLP"><img src="/themes/garland/minnelli/logo.png" alt="Brodies LLP" id="logo" /><span>Brodies LLP</span></a></h1>        </div>
-
-      </div> <!-- /header -->
+                  <a href="/" title="Home" rel="home" id="logo">
+            <img src="/sites/all/themes/brodies201612/logo.png" alt="Home" />
+          </a>
+        
+        <div id="name-and-slogan">
+          
+                  </div> <!-- /name-and-slogan -->
+      </div> <!-- /logo-title -->
 
       
-      <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
-          <h2>Site off-line</h2>                              <div class="clear-block">
+    </div> <!-- /header -->
+
+    <div id="container" class="clearfix">
+
+      
+      <div id="main" class="column"><div id="main-squeeze">
+
+        <div id="content">
+          <h1 class="title" id="page-title">Site under maintenance</h1>                    <div id="content-content" class="clearfix">
             <p>Brodies LLP is currently under maintenance. We should be back shortly. Thank you for your patience.</p>
-          </div>
-          <div id="footer"></div>
-      </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+          </div> <!-- /content-content -->
+        </div> <!-- /content -->
+
+      </div></div> <!-- /main-squeeze /main -->
 
       
     </div> <!-- /container -->
-  </div>
-<!-- /layout -->
 
-  </body>
+    <div id="footer-wrapper">
+      <div id="footer">
+              </div> <!-- /footer -->
+    </div> <!-- /footer-wrapper -->
+
+  </div> <!-- /page -->
+
+</body>
 </html>
