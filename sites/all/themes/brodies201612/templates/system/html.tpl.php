8e015c43a2af12f9e5b4c75851dc98de2fbcf202
Callum Elliot <callum@sakurabrae.co.uk>
2019-05-13 06:24:30 +0100

    GTM added to site;

--- sites/all/themes/brodies201612/templates/system/html.tpl.php
+++ sites/all/themes/brodies201612/templates/system/html.tpl.php
@@ -46,6 +46,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
     <head profile="<?php print $grddl_profile; ?>">
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-53WJ2RT');</script>
+      <!-- End Google Tag Manager -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <?php print $head; ?>
         <title><?php print $head_title; ?></title>
@@ -54,6 +61,10 @@
         <meta name="google-site-verification" content="QSV_lzQJ0VNBkE5Cd_oGD0wfrY5lZqSPmZ9RPDAOs68" />
     </head>
     <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53WJ2RT"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
         <div id="skip-link">
             <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
         </div>
