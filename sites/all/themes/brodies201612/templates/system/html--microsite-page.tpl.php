3b367bb4c15afc4cebc8fd0d705709e98126fc0e
Callum Elliot <callum@sakurabrae.co.uk>
2019-10-03 11:15:05 +0100

    Linked in JS added;

--- sites/all/themes/brodies201612/templates/system/html--microsite-page.tpl.php
+++ sites/all/themes/brodies201612/templates/system/html--microsite-page.tpl.php
@@ -1,45 +1,80 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
-<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
-<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
+<!--[if lt IE 7]>
+<html class="ie6 oldie"> <![endif]-->
+<!--[if IE 7]>
+<html class="ie7 oldie"> <![endif]-->
+<!--[if IE 8]>
+<html class="ie8 oldie"> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="">
-    <!--<![endif]-->
-    <head>
-      <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-53WJ2RT');</script>
-      <!-- End Google Tag Manager -->
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php print $head ?>
-        <title><?php print $head_title ?></title>
-        <?php print $styles ?>
-<?php print $scripts ?>   
-        <link href="/sites/all/themes/brodies201612/microsites/css/boilerplate.css" rel="stylesheet" type="text/css">
-        <link href="/sites/all/themes/brodies201612/microsites/css/microsites.css" rel="stylesheet" type="text/css">
-        <!--[if lt IE 9]>
-        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-        <script src="/sites/all/themes/brodies201612/microsites/js/respond.min.js"></script>
-        <script type="text/javascript">
-          function MM_jumpMenu(targ, selObj, restore) { //v3.0
-              eval(targ + ".location='" + selObj.options[selObj.selectedIndex].value + "'");
-              if (restore)
-                  selObj.selectedIndex = 0;
-          }
-        </script>
-    </head>
-    <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53WJ2RT"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-         <?php print $page; ?>
+<!--<![endif]-->
+<head>
+  <!-- Google Tag Manager -->
+  <script>(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-53WJ2RT');</script>
+  <!-- End Google Tag Manager -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php print $head ?>
+  <title><?php print $head_title ?></title>
+  <?php print $styles ?>
+  <?php print $scripts ?>
+  <link href="/sites/all/themes/brodies201612/microsites/css/boilerplate.css"
+        rel="stylesheet" type="text/css">
+  <link href="/sites/all/themes/brodies201612/microsites/css/microsites.css"
+        rel="stylesheet" type="text/css">
+  <!--[if lt IE 9]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <script
+    src="/sites/all/themes/brodies201612/microsites/js/respond.min.js"></script>
+  <script type="text/javascript">
+    function MM_jumpMenu(targ, selObj, restore) { //v3.0
+      eval(targ + ".location='" + selObj.options[selObj.selectedIndex].value + "'");
+      if (restore) {
+        selObj.selectedIndex = 0;
+      }
+    }
+  </script>
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53WJ2RT"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php print $page; ?>
 
-        <script type="text/javascript" src="/sites/all/themes/brodies201612/microsites/js/map-locations-dist.js"></script>
-    </body>
+<script type="text/javascript"
+        src="/sites/all/themes/brodies201612/microsites/js/map-locations-dist.js"></script>
+
+<!-- LinkedIn Insight -->
+<script type="text/javascript">
+  _linkedin_partner_id = "940353";
+  window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+  window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script><script type="text/javascript">
+  (function(){var s = document.getElementsByTagName("script")[0];
+    var b = document.createElement("script");
+    b.type = "text/javascript";b.async = true;
+    b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+    s.parentNode.insertBefore(b, s);})();
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=940353&fmt=gif" />
+</noscript>
+<!-- End LinkedIn Insight -->
+</body>
 </html>
